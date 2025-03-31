8cfe12862bc8e70106d20dfe90d42894514d67c5
adinocarti <200224@studierende.htl-donaustadt.at>
2025-03-31 11:12:49 +0200

    test

---
+++ loudticket/src/app/loudticket api/loudticketapi.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MonsterHunterApiService {
+  private baseUrl = 'https://app.ticketmaster.com/discovery/v2';
+
+  constructor(private http: HttpClient) {}
+
+  getAllEvents(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/discovery/v2/events`);
+  }
+
+
+
+}
