bde8fedfb354dd6cf17e6122d8777e8e4040bc71
rizi <200005@studierende-htl-donaustadt.at>
2025-04-22 13:31:24 +0200

    API connect

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
+
+https://www.figma.com/design/14nCqN5bzDiYgV4bLBcWw6/loudticket-eventwebseite?node-id=696-23686
+
 describe('AppComponent', () => {
   it('should create the app', async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [provideRouter([])]
     }).compileComponents();
-    
+
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
